e7c279460a3a672cd9beff8c2f292e84c86cae43
Nathan_BO <146062211+Noootzzz@users.noreply.github.com>
2024-02-11 17:25:53 +0100

    Init.

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("close");
+  nav.classList.toggle("reveal-nav");
+  menu.classList.toggle("reveal-items");
+});
+
+
